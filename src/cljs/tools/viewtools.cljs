eb41daac508d559bd55ec902132064885b7e393e
Ulf Ninow <mail@fnumatic.de>
2018-12-20 08:46:59 +0100

    feat: timing wrapper

--- src/cljs/tools/viewtools.cljs
+++ src/cljs/tools/viewtools.cljs
@@ -1,6 +1,22 @@
 (ns tools.viewtools
-  (:require [reitit.frontend.easy :as rtfe]))
+  (:require [reitit.frontend.easy :as rtfe]
+            [reagent.core :as rc]))
 
+(defn timing-wrapper [f attrs]
+  (let [start-time (rc/atom nil)
+        render-time (rc/atom nil)
+        now #(.now js/Date)
+        start #(reset! start-time (now))
+        stop #(reset! render-time (- (now) @start-time))
+        timed-f (with-meta f
+                  {:component-will-mount start
+                   :component-will-update start
+                   :component-did-mount stop
+                   :component-did-update stop})]
+    (fn []
+      [:div
+       [:p [:em "render time: " @render-time "ms"]]
+       [timed-f attrs]])))
 
 (defn item [e]
   (cond
